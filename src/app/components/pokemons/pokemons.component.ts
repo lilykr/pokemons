519faeda0bcd61111e5da032f02e4ec95eefcb76
lilykr <lisa-lou.k@hotmail.fr>
2020-12-12 16:43:55 +0100

    added select

--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Pokemon } from '../../../types'
 import jsonData from '../../../assets/seed.json'
+
 
 @Component({
   selector: 'pokemons',
@@ -9,15 +11,30 @@ import jsonData from '../../../assets/seed.json'
 })
 
 export class PokemonsComponent implements OnInit {
-  //initializing pokemons to an empty array 
+  // initializing pokemons to an empty array 
   pokemons: Pokemon[] = [];
+  filteredPokemons: Pokemon[] = [];
+  pokemonTypes: string[] = [];
 
   constructor() {
   }
 
   ngOnInit() {
-    //setting pokemon data
+    // setting pokemon data
     this.pokemons = jsonData as Pokemon[];
+    this.filteredPokemons = this.pokemons;
+
+    // setting pokemon types
+    const allTypes = this.pokemons.map(p => p.type).flat()
+    this.pokemonTypes = [...new Set(allTypes)];
+  }
+
+  onTypeChange(event: Event) {
+    const newType = (event.target as HTMLSelectElement).value
+    if (newType  === '')
+      this.filteredPokemons = this.pokemons;
+    else
+      this.filteredPokemons = this.pokemons.filter(p=> p.type.includes(newType))
   }
 
 }
